e1298f3386a73d3b2dd50c5b066f3fe5c15bca78
Nuel Agyei <nuelagyei992@gmail.com>
2023-09-20 08:56:56 +0000

    db seed

--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -2,8 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class UsersTableSeeder extends Seeder
 {
@@ -12,6 +14,23 @@ class UsersTableSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        //admin seeding
+        User::create([
+            'name'=>'Admin',
+            'email'=>'admin@NBC.com',
+            'role'=> 'admin',
+            'status' => 'active',
+            'password'=>Hash::make('password')
+        ]);
+
+
+        //admin seeding
+        User::create([
+            'name'=>'User',
+            'email'=>'user@NBC.com',
+            'role'=> 'user',
+            'status' => 'active',
+            'password'=>Hash::make('password')
+        ]);
     }
 }
