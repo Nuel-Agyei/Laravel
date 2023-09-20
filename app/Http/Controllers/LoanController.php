30e99508f6a3984c9c47d93d239ad7be77110cae
Nuel Agyei <nuelagyei992@gmail.com>
2023-09-20 11:41:59 +0000

    login and controller

--- app/Http/Controllers/LoanController.php
+++ app/Http/Controllers/LoanController.php
@@ -2,9 +2,22 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\LoanApplication;
+use App\Models\LoanType;
 use Illuminate\Http\Request;
 
 class LoanController extends Controller
 {
-    //
+    public function allLoanApplications()
+        {
+        $loan = LoanApplication::latest()->get();
+        return view('admin.loan_application.all', compact('loan'));
+        }
+
+    public function loanApplication()
+        {
+            $loanTypes = LoanType::all();
+            return view('user.loan_application.application');
+        }
+
 }
